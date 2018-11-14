d3817d3312d220d2a51263a1475a6bfa77695c8b
Michael <michaelcapawana@hotmail.com>
2018-11-14 13:57:50 -0700

    Prove 09

--- postalCalculator/server.js
+++ postalCalculator/server.js
@@ -8,41 +8,81 @@ app.set("view engine", "ejs");
 
 app.use(express.static('public'));
 
-app.get("/", function(req, res) {
-        console.log("A request came in for /");
-        var name = "John";
-        res.write("<h1>Welcome to the page, " + name + "</h1>");
-        res.end();
-    });
-
 app.get("/math", function(req, res){
-        handleMath(req, res);
+        calcRate(req, res);
     });
 
 
-function handleMath(req, res) {
+function calcRate(req, res) {
     var reqURL = url.parse(req.url, true);
-    var op = reqURL.query.operation;
-    var firstOp = Number(req.query.operand1);
-    var secondOp= Number(req.query.operand2);
-    console.log("A request came in for /math");
+    var type = reqURL.query.type;
+    var weight = Number(req.query.weight);
+    var newWeight = 0;
+    var price = 0;
 
-    op = op.toLowerCase();
-
-    var num = 0;
-
-    if(op == "add") {
-        num = firstOp + secondOp;
-    } else if(op == "subtract") {
-        num = firstOp - secondOp;
-    } else if(op == "multiply"){
-        num = firstOp * secondOp;
-    } else if(op == "divide"){
-        num = firstOp / secondOp;
+    if(type == "Letters(Stamped)") {
+        if(weight <= 3.5) {
+	    newWeight = Math.ceil(weight);
+	    if (newWeight == 1) {
+		price = 0.50;
+	    } else if (newWeight == 2) {
+		price = 0.71;
+	    } else if (newWeight == 3) {
+		price = 0.92;
+            } else if (newWeight == 4) {
+		price = 1.13;
+            }
+	} else {
+            price = "Sorry, no weights over 3.5 oz.";
+	}
+    } else if(type == "Letters(Metered)") {
+	if(weight <= 3.5) {
+            newWeight = Math.ceil(weight);
+            if (newWeight == 1){
+		price = 0.47;
+            } else if (newWeight == 2) {
+		price = 0.68;
+            } else if (newWeight == 3) {
+                price = 0.89;
+            } else if (newWeight == 4) {
+                price = 1.10;
+            }
+        } else {
+            price = "Sorry, no weights over 3.5 oz.";
+        }
+    } else if(type == "Large-Envelopes(Flats)"){
+	if(weight <= 13) {
+	    newWeight = Math.ceil(weight);
+	    price = (1.00 + (0.21 * (newWeight-1)));
+	} else {
+	    price = "Sorry, no weights over 13 oz.";
+	}
+	
+    } else if(type == "First-Class-Package-Service-Retail"){
+	if(weight <= 13) {
+            newWeight = Math.ceil(weight);
+            if (newWeight == 1 || newWeight == 2 || newWeight == 3 || newWeight == 4) {
+		price = 3.50;
+	    } else if (newWeight == 5 || newWeight == 6 || newWeight == 7 || newWeight == 8) {
+		price = 3.75;
+	    } else if (newWeight == 9){
+		price = 4.10;
+	    } else if (newWeight == 10){
+		price = 4.45;
+            } else if (newWeight == 11){
+		price = 4.80;
+            } else if (newWeight == 12){
+		price = 5.15;
+            } else if (newWeight == 13){
+		price = 5.50;
+            }
+        } else {
+            price = "Sorry, no weights over 13 oz.";
+		}
     } else {
         console.log("Error");
     }
-    res.render("results", {num:num});
+    res.render("results", {mType:type, weight:weight, price:price});
 
 }
 
