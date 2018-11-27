c2d3f9dcfb4dbd47480e43acdd38a00ef8cfde12
Michael <michaelcapawana@hotmail.com>
2018-11-27 13:00:36 -0700

    stuff

--- index.js
+++ index.js
@@ -22,29 +22,26 @@ const pool = new Pool({connectionString: connectionString});
 app.set('port', (process.env.PORT || 5000));                                                                                                                                       
 app.use(express.static(__dirname + '/public'));                                                                                                                                     
                                                                                                                                                                                     
-app.get('/getPerson', function(request, response) {                                                                                                                                 
-        getPerson(request, response);                                                                                                                                               
+app.get('/getBusiness', function(request, response) {                                                                                                                                 
+        getBusiness(request, response);                                                                                                                                               
     });                                                                                                                                                                              
                                                                                                                                                                                     
 app.listen(app.get('port'), function() {                                                                                                                                            
         console.log('Node app is running on port', app.get('port'));                                                                                                                
     });                                                                                                                                                                             
                                                                                                                                                                                     
-function getPerson(request, response) {                                                                                                                                             
-    var id = request.query.id;                                                                                                                                                      
-    getPersonFromDb(id, function(error, result) {                                                                                                                                   
-            if (error || result == null || result.length != 1) {                                                                                                                    
+function getBusiness(request, response) {                                                                                                                                               var id = request.query.id;                                                                                                                                                      
+    getBusinessFromDb(id, function(error, result) {                                                                                                                                     if (error || result == null || result.length != 1) {                                                                                                                    
                 response.status(500).json({success: false, data: error});                                                                                                           
             } else {                                                                                                                                                                
-                var person = result[0];                                                                                                                                             
-                response.status(200).json(result[0]);                                                                                                                               
+                var business = result[0];                                                                                                                                                           response.status(200).json(result[0]);                                                                                                                               
             }                                                                                                                                                                       
         });                                                                                                                                                                         
 }                                                                                                                                                                                   
                                                                                                                                                                                     
-function getPersonFromDb(id, callback) {                                                                                                                                            
-    console.log("Getting person from DB with id: " + id);                                                                                                                           
-    var sql = "SELECT id, first, last, birthdate FROM person WHERE id = $1::int";                                                                                                   
+function getBusinessFromDb(id, callback) {                                                                
+    console.log("Getting business from DB with id: " + id);
+    var sql = "SELECT id, name, score FROM business WHERE id = $1::int";                   
     var params = [id];                                                                                                                                                              
                                                                                                                                                                                     
     pool.query(sql, params, function(err, result) {                                                                                                                                 
