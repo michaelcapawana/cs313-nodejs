707a07721959a9b98febc839e12f98910a18787c
Michael <michaelcapawana@hotmail.com>
2018-11-29 15:56:20 -0700

    stuff

---
+++ public/teach11.js
@@ -0,0 +1,18 @@
+function searchMovie() {
+    var yourSearch = $("#txtSearch").val();
+    var params = {s: yourSearch, apikey:"byuidaho"};
+    $.get("http://www.omdbapi.com/", params, function(data, status){
+	    update(data)});
+}
+
+function update(data) {
+    if (data.Search && data.Search.length > 0) {
+	var results = $("#ulResults");
+	results.empty();
+	for (var i = 0; i < data.Search.length; i++) {
+	    var title = data.Search[i].Title;
+	    results.append("<li><p>" + title + "</p></li>");
+	}
+    }
+}
+
