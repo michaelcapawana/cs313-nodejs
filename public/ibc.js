a8881cb52ec615d8429d5252a0bbce1b5f52ce33
Michael <michaelcapawana@hotmail.com>
2018-11-29 10:08:26 -0700

    stuff

--- public/ibc.js
+++ public/ibc.js
@@ -1,11 +1,16 @@
 function displayBusinesses() 
 {
+    //alert("Does this Work?");
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
+	    alert("Does this Work?");
 	    document.getElementbyId("leftSide").innerHTML = this.responseText;
+	} else {
+	    alert(this.readyState);
+	    alert(this.status);
 	}
     };
     xhttp.open("GET", window.location.hostname + "/getBusiness", true);
-    xhtp.send();
+    xhttp.send();
 }
