1f88f1e317dd126cbbe0c71cd2f12533ed47ad46
Michael <michaelcapawana@hotmail.com>
2018-11-29 10:17:17 -0700

    stuff

--- public/ibc.js
+++ public/ibc.js
@@ -4,16 +4,16 @@ function displayBusinesses()
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
-	    alert("Does this Work?");
+	    //alert("Does this Work?");
 	    alert(this.responseText);
-	    document.getElementbyId("leftSide").innerHTML = this.responseText;
+	    document.getElementbyId("display").innerHTML = this.responseText;
 	    
 	} else {
-	    alert(this.readyState);
-	    alert(this.status);
+	    //alert(this.readyState);
+	    //alert(this.status);
 	}
     };
-    alert(window.location.hostname + "/getBusiness");
+    //alert(window.location.hostname + "/getBusiness");
     xhttp.open("GET", "/getBusiness", true);
     xhttp.send();
 }
--- public/ibc.js~
+++ public/ibc.js~
@@ -5,7 +5,9 @@ function displayBusinesses()
     xhttp.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
 	    alert("Does this Work?");
+	    alert(this.responseText);
 	    document.getElementbyId("leftSide").innerHTML = this.responseText;
+	    
 	} else {
 	    alert(this.readyState);
 	    alert(this.status);
