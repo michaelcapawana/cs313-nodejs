dc6baed9631d0b9bf83d512098ca562db3470b76
Michael <michaelcapawana@hotmail.com>
2018-11-29 17:56:18 -0700

    stuff

--- public/ibc.js
+++ public/ibc.js
@@ -14,7 +14,7 @@ function displayBusinesses()
 		//document.getElementById('link').href += myidtoinsert;
 
 		//txt += "<tr><td><a href='/getReviews?id=' >" + myObj[x].name + "</a></td></tr>";
-		txt += "<tr><td><a href='/getReviews?id=' onclick='location.href=this.href+'?key='+myidtoinsert;return false;' >" + myObj[x].name + "</a></td></tr>";
+		txt += "<tr><td><a href='' onclick='displayReviews()' >" + myObj[x].name + "</a></td></tr>";
 		//document.getElementById('link').href += myidtoinsert;
 	    }
 	    txt += "</table>"
@@ -27,3 +27,7 @@ function displayBusinesses()
     xhttp.send();
 }
 
+function displayReviews()
+{
+    alert("This worked!");
+}
