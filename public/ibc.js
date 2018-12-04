a53d24adb38b9bc998cdb6b034c1dda2c3dcd565
Michael <michaelcapawana@hotmail.com>
2018-12-04 09:44:10 -0700

    stuff

--- public/ibc.js
+++ public/ibc.js
@@ -1,32 +1,31 @@
 function displayBusinesses() 
 {
 
-
     var obj, dbParam, xmlhttp, myObj, x, txt = "";
     obj = { table: "customers", limit: 20 };
     dbParam = JSON.stringify(obj);
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-	alert(this.readyState);
-	alert(this.status);
         if (this.readyState == 4 && this.status == 200) {
-            alert("WE MADE IT!!!");
             myObj = JSON.parse(this.responseText);
-            alert(this.responseText);
-            txt += "<table border='1'>";
+            txt += "<table>"
             for (x in myObj) {
-                txt += "<tr><td>" + myObj[x].reviewer + "</td></tr>";
+                var myidtoinsert = Number(x) + 1 ;
+                //alert(myidtoinsert);                                                                                                                                               
+                //document.getElementById('link').href += myidtoinsert;                                                                                                              
+
+                //txt += "<tr><td><a href='/getReviews?id=' >" + myObj[x].name + "</a></td></tr>";                                                                                   
+                txt += "<tr><td><a href='' onclick='displayReviews()' >" + myObj[x].name + " - " + myObj[x].score + "</a></td></tr>";
+                //document.getElementById('link').href += myidtoinsert;                                                                                                              
             }
-            txt += "</table>";;
-            document.getElementById("displayReviews").innerHTML = txt;
+            txt += "</table>"
+            document.getElementById("displayBusiness").innerHTML = txt;
 
-	} else {
-	}
+        } else {
+        }
     };
-    xhttp.open("GET", "/getReviews", true);
+    xhttp.open("GET", "/getBusiness", true);
     xhttp.send();
-
-
 
 
 }
@@ -45,7 +44,7 @@ function displayReviews()
             alert("WE MADE IT!!!");
             myObj = JSON.parse(this.responseText);
 	    alert(this.responseText);
-	    txt += "<table border='1'>";
+	    txt += "<table>";
 	    for (x in myObj) {
 		txt += "<tr><td>" + myObj[x].reviewer + "</td></tr>";
 	    }
--- public/ibc.js~
+++ public/ibc.js~
@@ -1,30 +1,34 @@
 function displayBusinesses() 
 {
+
+
     var obj, dbParam, xmlhttp, myObj, x, txt = "";
     obj = { table: "customers", limit: 20 };
     dbParam = JSON.stringify(obj);
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-	if (this.readyState == 4 && this.status == 200) {
-	    myObj = JSON.parse(this.responseText);
-	    txt += "<table>"
-	    for (x in myObj) {
-		var myidtoinsert = Number(x) + 1 ;
-      		//alert(myidtoinsert);
-		//document.getElementById('link').href += myidtoinsert;
+	alert(this.readyState);
+	alert(this.status);
+        if (this.readyState == 4 && this.status == 200) {
+            alert("WE MADE IT!!!");
+            myObj = JSON.parse(this.responseText);
+            alert(this.responseText);
+            txt += "<table border='1'>";
+            for (x in myObj) {
+                txt += "<tr><td>" + myObj[x].reviewer + "</td></tr>";
+            }
+            txt += "</table>";;
+            document.getElementById("displayReviews").innerHTML = txt;
 
-		//txt += "<tr><td><a href='/getReviews?id=' >" + myObj[x].name + "</a></td></tr>";
-		txt += "<tr><td><a href='' onclick='displayReviews()' >" + myObj[x].name + " - " + myObj[x].score + "</a></td></tr>";
-		//document.getElementById('link').href += myidtoinsert;
-	    }
-	    txt += "</table>"
-	    document.getElementById("displayBusiness").innerHTML = txt;
-	    
 	} else {
 	}
     };
-    xhttp.open("GET", "/getBusiness", true);
+    xhttp.open("GET", "/getReviews", true);
     xhttp.send();
+
+
+
+
 }
 
 
